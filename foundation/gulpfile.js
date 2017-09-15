01870b05948f6ce1b6b233ef13b0040fc933deb0
Andy Wildisen <awildisen@gmx.ch>
2017-09-15 16:47:20 +0200

    initial

---
+++ foundation/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+var concat = require('gulp-concat');
+
+var sassPaths = [
+  'bower_components/normalize.scss/sass',
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+
+gulp.task('sass', function() {
+  return gulp.src(['scss/app.scss','scss/style.scss'])
+    .pipe($.sass({
+      includePaths: sassPaths
+      //,
+      //outputStyle: 'compressed' // if css compressed **file size**
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+
+gulp.task('app-css', function () {
+    return gulp.src(['css/app.css','css/style.css'])
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['css/**/*.css'], ['app-css']);
+});
